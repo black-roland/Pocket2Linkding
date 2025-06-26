393f522574202e6d8385c7119ef7e526394be2d8
kclark <kclark@autoverify.net>
2025-06-26 11:48:35 -0400

    Initial version

---
+++ Pocket2Linkding.py
@@ -0,0 +1,198 @@
+# /// script
+# requires-python = ">=3.9"
+# dependencies = [
+#   "attrs",
+#   "unidecode",
+# ]
+# ///
+"""
+Migrate Mozilla Pocket to linkding.
+
+This uses the Pocket export format as input and creates .html bookmark files that can
+be directly imported into linkding.  Will preserve the date the link was added to
+Pocket and any Pocket tags.
+
+See https://github.com/hkclark/Pocket2Linkding for more information.
+"""
+
+from __future__ import annotations
+
+import argparse
+import csv
+from datetime import datetime
+from pathlib import Path
+from typing import Iterator
+
+
+import attrs
+from unidecode import unidecode
+
+
+@attrs.frozen
+class Config:
+    """Configuration via cmd-line args."""
+
+    _parser: argparse.ArgumentParser = attrs.field(
+        init=False,
+        validator=attrs.validators.instance_of(argparse.ArgumentParser),
+    )
+    """The parser."""
+
+    @_parser.default
+    def _parser_default(self):
+        """Build the _parser attribute."""
+        parser = argparse.ArgumentParser(
+            description="Migrate from Mozilla Pocket to Linkding"
+        )
+        parser.add_argument(
+            "--pocket_export_dir",
+            "-d",
+            help="Directory containing .csv file(s) exported from Pocket",
+            type=str,
+            required=True,
+        )
+        return parser
+
+    _parsed_args: argparse.Namespace = attrs.field(
+        init=False,
+        default=attrs.Factory(
+            takes_self=True,
+            factory=lambda self: self._parser.parse_args(),
+        ),
+        validator=attrs.validators.instance_of(argparse.Namespace),
+    )
+    """Results of argparse.parse_args()."""
+
+    pocket_export_path: Path = attrs.field(
+        init=False,
+        default=attrs.Factory(
+            takes_self=True,
+            factory=lambda self: Path(self._parsed_args.pocket_export_dir),
+        ),
+    )
+    """Pocket Export Dir as a ready-to-use pathlib Path obj."""
+
+    @pocket_export_path.validator
+    def _pocket_export_path_validator(self, attrib, value):
+        """Make sure the Pocket Export Dir looks right."""
+        if not value.exists():
+            raise ValueError(f"Invalid pocket-export-dir: '{value}'")
+        if not value.is_dir():
+            raise ValueError(f"pocket-export-dir must be a directory: '{value}'")
+
+
+@attrs.frozen
+class BookmarkFile:
+    """A bookmark HTML file in the format used by Linkding."""
+
+    header: str = attrs.field(
+        init=False,
+        default=(
+            "<!DOCTYPE NETSCAPE-Bookmark-file-1>\n"
+            '<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">\n'
+            "<TITLE>Bookmarks</TITLE>\n\n<H1>Bookmarks</H1>\n\n<DL><p>\n\n"
+        ),
+        validator=attrs.validators.instance_of(str),
+    )
+
+    footer: str = attrs.field(
+        init=False,
+        default="\n\n</DL><p>\n",
+        validator=attrs.validators.instance_of(str),
+    )
+
+    def write_file(self, csv_file_path: Path, csv_records: list[CSVRecord]):
+        """Write out all of the csv_records to the html file."""
+        html_file_path = csv_file_path.with_suffix(".html")
+        with html_file_path.open(mode="w", encoding="utf-8") as f:
+            f.write(self.header)
+            for csv_record in csv_records:
+                f.write(csv_record.as_html)
+            f.write(self.footer)
+
+
+@attrs.frozen
+class CSVRecord:
+    """One row/record of a CSV file."""
+
+    title: str = attrs.field(
+        validator=attrs.validators.instance_of(str),
+    )
+
+    url: str = attrs.field(
+        validator=attrs.validators.instance_of(str),
+    )
+
+    time_added: int = attrs.field(
+        converter=int,
+        validator=attrs.validators.instance_of(int),
+    )
+
+    tags: list = attrs.field(
+        converter=lambda value: value.split("|") if value else [],
+        validator=attrs.validators.instance_of(list),
+    )
+
+    status: str = attrs.field(
+        validator=attrs.validators.instance_of(str),
+    )
+
+    dt_time_added: datetime = attrs.field(
+        init=False,
+        default=attrs.Factory(
+            takes_self=True,
+            factory=lambda self: datetime.fromtimestamp(self.time_added),
+        ),
+        validator=attrs.validators.instance_of(datetime),
+    )
+
+    @property
+    def clean_title(self):
+        return unidecode(self.title)
+
+    @property
+    def clean_url(self):
+        return unidecode(self.url.strip().replace(" ", "%20"))
+
+    @property
+    def as_html(self):
+        return (
+            f'\n<DT><A HREF="{self.clean_url}" ADD_DATE="{self.time_added}" '
+            f'LAST_MODIFIED="{self.time_added}" PRIVATE="1" TOREAD="1" '
+            f'TAGS="{",".join(self.tags)}">{self.clean_title}</A>\n'
+        )
+
+
+@attrs.define
+class CSVFile:
+    """A CSV file (as exported from Mozilla Pocket)."""
+
+    file_path: Path = attrs.field(
+        validator=attrs.validators.instance_of(Path),
+    )
+
+    def row_iterator(self) -> Iterator[list[str]]:
+        """Context-managed generator that yields rows from the CSV file."""
+        with self.file_path.open(mode="r", newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                yield row
+
+    def get_csv_records(self) -> list[CSVRecord]:
+        return [CSVRecord(**row_dict) for row_dict in self.row_iterator()]
+
+    def __str__(self):
+        return str(self.file_path)
+
+
+def main():
+    cfg = Config()
+    for csv_file_path in cfg.pocket_export_path.glob("*.csv"):
+        csv_file = CSVFile(file_path=csv_file_path)
+        csv_records = csv_file.get_csv_records()
+        print(f"Got {len(csv_records)} CSV Records for {csv_file}")
+        BookmarkFile().write_file(csv_file_path=csv_file_path, csv_records=csv_records)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # Pocket2Linkding
-Simple tool to migrate from Mozilla Pocket to Linkding
+
+Simple tool to migrate from [Mozilla Pocket](https://getpocket.com/home) to
+[linkding](https://linkding.link/).
+
+## Purpose
+
+Like many others, I needed to find a new home for my bookmarks when Mozilla announced on
+May 22, 2025 that they are [shutting down Pocket](https://support.mozilla.org/en-US/kb/future-of-pocket)
+as of July 8, 2025.  I wanted something self-hosted.  After looking at many different options
+(see [awesome-selfhosted](https://github.com/awesome-selfhosted/awesome-selfhosted?tab=readme-ov-file#bookmarks-and-link-sharing)),
+I decided to switch to [linkding](https://linkding.link/) ([GitHub](https://github.com/sissbruecker/linkding)).
+I put together a quick Python-based migration tool to convert from the
+[Pocket export format](https://support.mozilla.org/en-US/kb/exporting-your-pocket-list) to
+linkding's import format while preserving the original time the link was added to Pocket
+and any tags that you have set.
+
+Note that while Pocket is shutting down on July 8, 2025, Mozilla will keep the
+[export](https://support.mozilla.org/en-US/kb/exporting-your-pocket-list) service up until
+October 8, 2025.
+
+## Usage
+
+While there are many ways to run Python scripts, this should be enough to get you going:
+
+## Create, Download, and Unzip the Mozilla Pocket Export
+
+* Use the [Pocket export](https://support.mozilla.org/en-US/kb/exporting-your-pocket-list) to
+    initiate an export.
+* Mozilla will send you a link to download the CSV export once it's ready.  Download the
+    `pocket.zip` file and unzip it on your local system.: `unzip pocket.zip`
+    * The `pocket.zip` file contains one or more files with names like `part_000000.csv`.
+    * If you have more than 10,000 bookmmarks, there will be multiple files with 10K bookmarks
+        in each (e.g., `part_000000.csv`, `part_000001.csv`, etc.)
+
+## Run the Migration Tool
+
+Use the migration tool to convert each `part_######.csv` file into a `part_######.html` bookmark
+file that can be imported into linkding.
+
+### Using Python [uv](https://docs.astral.sh/uv/)
+
+* Install uv if you don't have it already
+    * See [Installing uv](https://docs.astral.sh/uv/getting-started/installation/) for details,
+        but they recommend `curl -LsSf https://astral.sh/uv/install.sh | sh`.
+    * Note that many people have legitimate security concerns about using curl to pipe commands
+        into the shell in this manner.  See the "Installing uv" link above for alternatives.
+* View the help info: `uv run Pocket2Linkding.py -h`
+* Run the migration: `uv run Pocket2Linkding -d .`
+
+### Using a Python Virtual Environment
+
+I'm skipping the normal virtual environment activation step here, but feel free to
+[include that if you want](https://docs.python.org/3/library/venv.html#how-venvs-work).
+
+* Create a Python Virtual Environment: `python3 -m venv venv`
+* Install dependencies: `./venv/bin/pip3 install attrs unidecode`
+* View the help info: `./venv/bin/python Pocket2Linkding.py -h`
+* Run the migration: `./venv/bin/python Pocket2Linkding.py -d .`
+
+Or, for Windows:
+
+* Create a Python Virtual Environment: `py -3 -m venv venv`
+* Install dependencies: ` .\venv\Scripts\pip.exe install attrs unidecode`
+* View the help info: `.\venv\Scripts\python.exe .\Pocket2Linkding.py -h`
+* Run the migration: `.\venv\Scripts\python.exe .\Pocket2Linkding.py -d .`
+
+
+## Import Into linkding
+
+* [Install linkding](https://linkding.link/installation/) if you haven't already.  I recommend
+    using [Docker](https://www.docker.com/) and using the `latest-plus` image to get the
+    built-in ability to archive pages using
+    [SingleFile](https://github.com/gildas-lormeau/single-file-cli).
+* Open your instance of linkding.
+* Go to Settings -> General.
+* Near the bottom of the page you will see an "Import" section.
+* Click the "Browse" button and select a .html file.
+* Click "Upload".
+* Repeat for each file if you have more than 10K bookmarks saved in Pocket.
